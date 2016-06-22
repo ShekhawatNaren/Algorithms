69dfb98f3e0798255102bf4d4f3a9180cb4e1eb8
ShekhawatNaren <narendra.shekhawat33@gmail.com>
2016-06-22 20:24:56 +0530

    Added nth smallest element algo

--- Algorithms.h
+++ Algorithms.h
@@ -19,18 +19,42 @@ namespace MyAlgos {
 				++smallIter;
 			}
 		}
-		std::swap(*(smallIter), *_Last);
+		std::swap(*smallIter, *_Last);
 		return smallIter;
 	}
 
+	/*
+	Takes first and last iterator of the container and sorts the data for [first,last] (inclusive) range
+	*/
 	template<typename RandomAccessIterator>
 	void qsort(RandomAccessIterator _First, RandomAccessIterator _Last) {
 		if (_First < _Last) {
 			RandomAccessIterator pIter = randomized_partition(_First, _Last);
 			if(pIter>_First)
-				qsort(_First, pIter - 1);
+				qsort(_First, --pIter);
 			if(pIter<_Last)
-				qsort(pIter + 1, _Last);
+				qsort(++pIter, _Last);
+		}
+	}
+
+	/*
+	Takes first and last iterator of the container and finds the nth(index starts at 0th) smallest
+	element in range [first,last] (inclusive)
+	*/
+	template<typename RandomAccessIterator>
+	RandomAccessIterator nth_element(RandomAccessIterator _First, RandomAccessIterator _Last, size_t n) {
+		if ((_Last - _First) < n)
+			return _Last;
+
+		RandomAccessIterator pIter = randomized_partition(_First, _Last);
+		if (pIter == _First + n) {
+			return pIter;
+		}
+		else if (pIter < _First + n) {
+			return MyAlgos::nth_element(pIter + 1, _Last, n - (pIter - _First) - 1);
+		}
+		else {
+			return MyAlgos::nth_element(_First, pIter - 1, n);
 		}
 	}
 }
