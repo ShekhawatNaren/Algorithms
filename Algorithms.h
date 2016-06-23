e86e9f4273a2009df8d47d8cace88f28b62976f4
ShekhawatNaren <narendra.shekhawat33@gmail.com>
2016-06-23 22:40:19 +0530

    insertion sort algo

--- Algorithms.h
+++ Algorithms.h
@@ -3,6 +3,7 @@ using namespace std;
 namespace MyAlgos {
 	template<typename RandomAccessIterator>
 	RandomAccessIterator randomized_partition(RandomAccessIterator _First, RandomAccessIterator _Last) {
+		//Find the random partition element
 		size_t distance = _Last - _First;
 		RandomAccessIterator randomIter = _Last;
 		if (distance <= 0)
@@ -12,6 +13,7 @@ namespace MyAlgos {
 		}
 		std::swap(*randomIter, *_Last);
 		
+		//Partition the container around the partition element
 		RandomAccessIterator smallIter = _First;
 		for (; _First <= _Last-1; ++_First) {
 			if (*_First <= *_Last) {
@@ -25,15 +27,16 @@ namespace MyAlgos {
 
 	/*
 	Takes first and last iterator of the container and sorts the data for [first,last] (inclusive) range
+	Inplace algorithm
 	*/
 	template<typename RandomAccessIterator>
 	void qsort(RandomAccessIterator _First, RandomAccessIterator _Last) {
-		if (_First < _Last) {
+		while (_First < _Last) {
 			RandomAccessIterator pIter = randomized_partition(_First, _Last);
-			if(pIter>_First)
-				qsort(_First, --pIter);
-			if(pIter<_Last)
-				qsort(++pIter, _Last);
+			if (pIter>_First)
+				qsort(_First, pIter - 1);
+
+			_First = pIter + 1; //tail recursive
 		}
 	}
 
@@ -55,6 +58,32 @@ namespace MyAlgos {
 		}
 		else {
 			return MyAlgos::nth_element(_First, pIter - 1, n);
+		}
+	}
+
+	/*
+	insertion sort algorithm (in-place)
+	*/
+	template<typename BidirectionalIterator>
+	void insertion_sort(BidirectionalIterator _First, BidirectionalIterator _Last) {
+		if (_First == _Last)
+			return;
+		BidirectionalIterator right = std::next(_First);
+		for (; right <= _Last; ++right) {
+			BidirectionalIterator left = std::prev(right);
+			auto key = *right;
+			bool flag = true;
+			while( flag && left >= _First && *left > key) {
+				*(left + 1) = *left;
+				if (left != _First)
+					--left;
+				else
+					flag = false;
+			}
+			if (flag)
+				*(left + 1) = key;
+			else
+				*left = key;
 		}
 	}
 }
